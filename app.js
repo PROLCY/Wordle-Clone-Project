656b85eda55de4fd11facbd6cb5e85a3ed30942e
PROLCY <ckd248@naver.com>
2022-10-30 15:40:25 +0900

    2022/10/30 프론트와 백엔드 결합

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('port', process.env.PORT || 4000);
 connect();
 
 app.use(morgan('dev'));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello, Express');
+    res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 module.exports = router;
